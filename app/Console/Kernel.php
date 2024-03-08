1ba78d75b1c50810d1938abdbda7c9fba0a5418d
avavion <avavionmvm@gmail.com>
2024-03-08 14:14:17 +0300

    Add: files

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command(AggregateProjects::class)
-            ->daily()
+            ->everyThirtyMinutes()
             ->runInBackground();
     }
 
